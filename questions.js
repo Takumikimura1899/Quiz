1dc44542a764534be87649a41f6b865e0c0e278e
Takumikimura141899 <kimura141899@gmail.com>
2022-01-21 07:12:39 +0900

    [add] js function alert

---
+++ questions.js
@@ -0,0 +1,7 @@
+const answersList = document.querySelectorAll('ol.answers li');
+
+answersList.forEach((li) => li.addEventListener('click', checkClickedAnswer));
+
+function checkClickedAnswer() {
+  alert('clicked!');
+}
