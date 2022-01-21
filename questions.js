0fba54ed13794f050f20b4d4c6493d170f7d3426
Takumikimura141899 <kimura141899@gmail.com>
2022-01-21 20:32:52 +0900

    [add] event.currentTarget

--- questions.js
+++ questions.js
@@ -2,14 +2,19 @@ const answersList = document.querySelectorAll('ol.answers li');
 
 answersList.forEach((li) => li.addEventListener('click', checkClickedAnswer));
 
-function checkClickedAnswer() {
+function checkClickedAnswer(event) {
+  const clickedAnswerElement = event.currentTarget;
+  console.log(clickedAnswerElement.dataset.answer);
+  const selectedAnswer = clickedAnswerElement.dataset.answer;
+
+  const correctAnswer = 'B';
   //  メッセージを入れる変数を用意
   let message;
   // カラーコードを入れる変数を用意
   let answerColorCode;
 
   // 答えが正しいか判定
-  if (false) {
+  if (selectedAnswer === correctAnswer) {
     // 正しい答えだった時
     message = '正解です!おめでとう!';
     answerColorCode = '';
@@ -19,7 +24,7 @@ function checkClickedAnswer() {
     answerColorCode = '#f05959';
   }
 
-  alert('clicked!');
+  alert(message);
 
   // 色を変更
   document.querySelector('span#correct-answer').style.color = '#f05959';
