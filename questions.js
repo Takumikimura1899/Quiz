2fccae791680206e0686373d7ba28d21d0fc6172
Takumikimura141899 <kimura141899@gmail.com>
2022-01-21 20:49:29 +0900

    javascript quiz finish

--- questions.js
+++ questions.js
@@ -2,12 +2,23 @@ const answersList = document.querySelectorAll('ol.answers li');
 
 answersList.forEach((li) => li.addEventListener('click', checkClickedAnswer));
 
+const correctAnswers = {
+  question1: 'B',
+  question2: 'A',
+  question3: 'B',
+  question4: 'C',
+  question5: 'D',
+  question6: 'B',
+};
+
 function checkClickedAnswer(event) {
   const clickedAnswerElement = event.currentTarget;
   console.log(clickedAnswerElement.dataset.answer);
   const selectedAnswer = clickedAnswerElement.dataset.answer;
 
-  const correctAnswer = 'B';
+  const questionId = clickedAnswerElement.closest('ol.answers').dataset.id;
+
+  const correctAnswer = correctAnswers[questionId];
   //  メッセージを入れる変数を用意
   let message;
   // カラーコードを入れる変数を用意
@@ -27,7 +38,7 @@ function checkClickedAnswer(event) {
   alert(message);
 
   // 色を変更
-  document.querySelector('span#correct-answer').style.color = '#f05959';
+  document.querySelector('span#correct-answer').style.color = answerColorCode;
   //   答え全体を表示
   document.querySelector('div#section-correct-answer').style.display = 'block';
 }
