0c9dfa83967afa0134de697915f67d574f90927d
Takumikimura141899 <kimura141899@gmail.com>
2022-01-28 06:18:14 +0900

    [add] 404ページを表示する際にheaderのステータスをエラーコード404に

--- html/question.php
+++ html/question.php
@@ -7,6 +7,12 @@ $id = escape($_GET["id"]);
 $data = fetchById($id);
 
 if (!$data) {
+    // HTTPレスポンスのヘッダを404にする
+    header("HTTP/1.1 404 Not Found");
+
+    // レスポンスの種類を指定する
+    header("Content-Type: text/html; charset=UTF-8");
+
     include __DIR__ . "/../template/404.tpl.php";
     exit(0);
 }
