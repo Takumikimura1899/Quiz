4a3e3634f944b2b91d209c29058c9c1703cdc590
Takumikimura141899 <kimura141899@gmail.com>
2022-01-27 21:50:17 +0900

    [add] htmlspecialcharsでエスケープ処理

--- html/question.php
+++ html/question.php
@@ -6,17 +6,17 @@ $id = "1";
 
 $data = fetchById($id);
 
-$question = nl2br($data[1]);
+$question = nl2br(htmlspecialchars($data[1]));
 
 // <li data-answer="A">HyperTextMakingLanguage</li>
 // <li data-answer="B">HyperTextMarkupLanguage</li>
 // <li data-answer="C">HonmaniTensaitekinaMajidesugoiLanguage</li>
 // <li data-answer="D">そもそも略称ではない</li>
 $answers = [
-    "A" => $data[2],
-    "B" => $data[3],
-    "C" => $data[4],
-    "D" => $data[5],
+    "A" => htmlspecialchars($data[2]),
+    "B" => htmlspecialchars($data[3]),
+    "C" => htmlspecialchars($data[4]),
+    "D" => htmlspecialchars($data[5]),
 ];
 
 //  <p>
@@ -24,8 +24,8 @@ $answers = [
 //           これが間違えてたら「HTMLとは？」の動画を復習お願いします！
 //         </p>
 
-$correctAnswer = strtoupper($data[6]);
+$correctAnswer = htmlspecialchars(strtoupper($data[6]));
 $correctAnswerValue = $answers[$correctAnswer];
-$explanation = nl2br($data[7]);
+$explanation = nl2br(htmlspecialchars($data[7]));
 
 include __DIR__ . "/../template/question.tpl.php";
