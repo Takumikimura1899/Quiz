5765897c56ec9dbb5d56839fc100f9d897e65cf0
Takumikimura141899 <kimura141899@gmail.com>
2022-01-27 22:11:55 +0900

    [add] 共通の処理を関数化

--- html/question.php
+++ html/question.php
@@ -6,26 +6,12 @@ $id = "1";
 
 $data = fetchById($id);
 
-$question = nl2br(htmlspecialchars($data[1]));
+$formattedData = generateFormattedData($data);
 
-// <li data-answer="A">HyperTextMakingLanguage</li>
-// <li data-answer="B">HyperTextMarkupLanguage</li>
-// <li data-answer="C">HonmaniTensaitekinaMajidesugoiLanguage</li>
-// <li data-answer="D">そもそも略称ではない</li>
-$answers = [
-    "A" => htmlspecialchars($data[2]),
-    "B" => htmlspecialchars($data[3]),
-    "C" => htmlspecialchars($data[4]),
-    "D" => htmlspecialchars($data[5]),
-];
-
-//  <p>
-//           <span id="correct-answer">B. HyperTextMarkupLanguage</span><br />
-//           これが間違えてたら「HTMLとは？」の動画を復習お願いします！
-//         </p>
-
-$correctAnswer = htmlspecialchars(strtoupper($data[6]));
+$question = $formattedData["question"];
+$answers = $formattedData["answers"];
+$correctAnswer = $formattedData["correctAnswer"];
 $correctAnswerValue = $answers[$correctAnswer];
-$explanation = nl2br(htmlspecialchars($data[7]));
+$explanation = $formattedData["explanation"];
 
 include __DIR__ . "/../template/question.tpl.php";
